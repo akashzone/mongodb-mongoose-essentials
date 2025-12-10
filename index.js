4d313be313c1febf41a72513edfe1e8e9fe28cfa
akashzone <akashnadar.dev@gmail.com>
2025-12-10 14:52:08 +0530

    add update examples using updateOne and updateMany

--- index.js
+++ index.js
@@ -20,22 +20,40 @@ const userSchema = mongoose.Schema({
 //Created Model (Collection) in mongoose.
 const User = mongoose.model("User", userSchema);
 
-User.insertMany([{
-  name : "Jones",
-  email : "jones@gmail.com",
-  age : 19,
-}, {
-  name : "Govind",
-  email : "govind@gmail.com",
-  age : 21
-}, {
-  name : "Praviin",
-  email : "pravinn@gmail.com",
-  age: 21
-}]).then((res)=>{
+// User.insertMany([{
+//   name : "Jones",
+//   email : "jones@gmail.com",
+//   age : 19,
+// }, {
+//   name : "Govind",
+//   email : "govind@gmail.com",
+//   age : 21
+// }, {
+//   name : "Praviin",
+//   email : "pravinn@gmail.com",
+//   age: 21
+// }]).then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+
+// User.find({age : {$gte : 20}}).then((res)=>{
+//   console.log(res[0].name);
+// }).catch((err)=>{
+//   console.log(err);
+// });
+
+
+// User.find({_id : "69393722da82c11c8b659192"}).then((res)=>{
+//    console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+User.findByIdAndUpdate("69393722da82c11c8b659192",{age : 199},{new : true}).then((res)=>{
   console.log(res);
 }).catch((err)=>{
   console.log(err);
-})
-
-
+});
