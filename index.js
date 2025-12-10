5e041cebf269f8c14bbe7acf9803b8751b33a823
akashzone <akashnadar.dev@gmail.com>
2025-12-10 14:27:50 +0530

    add mongoose schemas for users add example script for inserting a single document in mongoose

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+// Created Schema using mongoose.
+const userSchema = mongoose.Schema({
+  name: String,
+  email : String,
+  age : Number
+})
+
+//Created Model (Collection) in mongoose.
+const User = mongoose.model("User",userSchema);
+
+// Created a document (Object) of User class.
+const user1 = new User({
+  name : "Sam",
+  email : "SamJoshua2007@yahoo.com",
+  age : 18
+})
+
+// Saving it into the DB using save().
+user1.save().then((res)=>{
+  console.log(res);
+}).catch((err)=>{
+  console.log(err);
+})
